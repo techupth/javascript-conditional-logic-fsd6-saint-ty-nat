7ba7cac559ac09e8e7962eee56ce4e1d0ebf7825
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-05-15 14:12:31 +0700

    feat:done all exercises

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,14 @@
 //Exercise 3
 // Ternary Operator
-let lightBulbStatus = "Off";
+let lightBulbStatus = "On";
 
 //Start coding here
+
+let lightBulbStatusCheck =
+  lightBulbStatus === "On"
+    ? "Light bulb is On."
+    : lightBulbStatus === "Off"
+    ? "Light bulb is Off."
+    : "Please choose the correct input (On/Off)";
+
+console.log(lightBulbStatusCheck);
